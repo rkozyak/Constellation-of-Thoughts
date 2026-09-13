988c46c94ac09416a6ce902fcaf06f3773a0a88f
Richard <61517945+rkozyak@users.noreply.github.com>
2026-09-12 22:33:14 -0400

    Initial Commit

---
+++ Constellation of Thoughts/Constellation_of_ThoughtsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Constellation_of_ThoughtsApp.swift
+//  Constellation of Thoughts
+//
+//  Created by Richard Kozyak on 9/12/26.
+//
+
+import SwiftUI
+
+@main
+struct Constellation_of_ThoughtsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
